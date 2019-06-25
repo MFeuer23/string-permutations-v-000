af38479c3f8c87e7d5c604fdbb3072f0471c8c1e
Mallory Feuer <mallory.feuer@gmail.com>
2019-06-25 19:43:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 function findAllPermutations(str) {
-  let result = []
-  for (let i = 0; i < str.length; i++) {
-    result.push(``)
+  if (string.length < 2) { // This is our break condition
+    return string;
   }
-}
 
+  var permutations = []; 
+
+  for (var i=0; i<string.length; i++) {
+    var char = string[i];
+
+    // Cause we don't want any duplicates:
+    if (string.indexOf(char) != i) // if char was used already
+      continue;           // skip it this time
+
+    var remainingString = string.slice(0,i) + string.slice(i+1,string.length); //Note: you can concat Strings via '+' in JS
+
+    for (var subPermutation of permut(remainingString))
+      permutations.push(char + subPermutation)
+
+    }
+    return permutations;
+}
